1b0a0a93c0d34c76d47edede746a4a2b2bfd0a68
Michel H <devMichelH@gmail.com>
2025-04-09 07:12:47 -0300

    first commit

---
+++ resources/views/livewire/users/edit.blade.php
@@ -0,0 +1,102 @@
+<?php
+
+use App\Models\User; 
+use App\Models\Country;
+use App\Models\Language;
+use Livewire\Volt\Component;
+use Livewire\Attributes\Rule; 
+use Livewire\WithFileUploads;
+use Mary\Traits\Toast;
+
+new class extends Component {
+    //We will use it later
+    use Toast, WithFileuploads;
+
+    //Component parameter
+    public User $user;
+
+    #[Rule('required')]
+    public string $name = '';
+
+    #[Rule('required|email')]
+    public string $email = '';
+
+    #[Rule('sometimes')]
+    public ?int $country_id = null;
+
+    #[Rule('nullable|image|max:1024')]
+    public $photo;
+
+    #[Rule('required')]
+    public array $my_languages = [];
+
+    #[Rule('sometimes')]
+    public ?string $bio = null;
+
+    public function mount(): void
+    {
+        $this->fill($this->user);
+
+        $this->my_languages = $this->user->languages->pluck('id')->all();
+    }
+
+    public function save(): void
+    {
+        $data = $this->validate();
+
+        $this->user->update($data);
+
+        $this->user->languages()->sync($this->my_languages);
+
+        if ($this->photo) {
+            $url = $this->photo->store('users', 'public');
+            $this->user->update(['avatar' => "/storage/$url"]);
+        }
+
+        $this->success('User updated with success.', redirectTo: '/users');
+    }
+
+    public function with(): array
+    {
+        return [
+            'countries' => Country::all(),
+            'languages' => Language::all(),
+        ];
+    }
+};
+?>
+
+<div>
+    <x-header title="Update {{ $user->name }}" separator />
+        
+        <div class="grid gap-5 lg:grid-cols-2">
+            <div>
+                <x-form wire:submit="save">
+                    
+                    <x-file label="Avatar" wire:model="photo" accept="image/png, image/jpeg" crop-after-change>
+                        <img src="{{ $user->avatar ?? '/empty-user.jpg' }}" class="h-36 rounded-lg">
+                    </x-file>
+
+                    <x-input label="Name" wire:model="name" />
+                    <x-input label="Email" wire:model="email" />
+                    <x-select label="country" wire:model="country_id" :options="$countries" placeholder="---" />
+
+                    <x-choices-offline
+                        label="My Languages"
+                        wire:model="my_languages"
+                        :options="$languages"
+                        serachable />
+                    
+                    <x-editor wire:model="bio" label="Biography" hint="the great biography" />
+
+                    <x-slot:actions>
+                        <x-button label="Cancel" link="/users" />
+                        <x-button label="Save" icon="o-paper-airplane" spinner="save" type="submit" class="btn-primary" />
+                    </x-slot:actions>
+                </x-form>
+            </div>
+            <div>
+                <img src="/storage/users/img/control-pana.png" width="300" class="mx-auto">
+            </div>
+        </div>
+</div>
---
+++ resources/views/livewire/users/index.blade.php
@@ -0,0 +1,139 @@
+<?php
+
+use App\Models\User;
+use App\Models\Country;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Volt\Component;
+use Mary\Traits\Toast;
+use Livewire\WithPagination;
+
+new class extends Component {
+    use WithPagination;
+
+    use Toast;
+
+    public string $search = '';
+
+    public bool $drawer = false;
+
+    public array $sortBy = ['column' => 'id', 'direction' => 'asc'];
+
+    // Clear filters
+    public function clear(): void
+    {
+        $this->reset();
+        $this->resetPage(); 
+        $this->success('Filters cleared.', position: 'toast-bottom');
+    }
+
+    // Delete action
+    // public function delete($id): void
+    // {
+    //     $this->warning("Will delete #$id", 'It is fake.', position: 'toast-bottom');
+    // }
+
+    public function delete(User $user): void
+    {
+        $user->delete();
+        $this->warning("$user->name deleted", 'Good bye!', position: 'toast-bottom');
+    }
+
+    // Table headers
+    public function headers(): array
+    {
+        return [
+            ['key' => 'avatar', 'label' => '', 'class' => 'w-1'],
+            ['key' => 'id', 'label' => '#', 'class' => 'w-1'],
+            ['key' => 'name', 'label' => 'Name', 'class' => 'w-64'],
+            ['key' => 'country_name', 'label' => 'Country', 'class' => 'hidden lg:table-cell'],
+            ['key' => 'email', 'label' => 'E-mail', 'sortable' => false],
+        ];
+    }
+
+    /**
+     * For demo purpose, this is a static collection.
+     *
+     * On real projects you do it with Eloquent collections.
+     * Please, refer to maryUI docs to see the eloquent examples.
+     */
+    
+//     public function users(): Collection
+// {
+//     return User::query()
+//         ->with(['country'])
+//         ->withAggregate('country', 'name')
+//         ->when($this->search, fn(Builder $q) => $q->where('name', 'like', "%$this->search%"))
+//         ->orderBy(...array_values($this->sortBy))
+//         ->get();
+// }
+
+    public function users(): LengthAwarePaginator
+{
+    return User::query()
+        ->withAggregate('country', 'name')
+        ->when($this->search,     fn(Builder $q) => $q->where('name', 'like', "%$this->search%"))
+        ->when($this->country_id, fn(Builder $q) => $q->where('country_id', $this->country_id))
+        ->orderBy(...array_values($this->sortBy))
+        ->paginate(5);
+}
+
+    public function with(): array
+    {
+        return [
+            'users'     => $this->users(),
+            'headers'   => $this->headers(),
+            'countries' => Country::all(),
+        ];
+    }
+
+    public function updated($property): void
+    {
+        if (! is_array($property) && $property != "") {
+            $this->resetPage();
+        }
+    }
+
+    //Create a public property
+    public int $country_id =0;
+
+} 
+
+?>
+
+<div>
+    <!-- HEADER -->
+    <x-header title="Users" separator progress-indicator>
+        <x-slot:middle class="!justify-end">
+            <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
+        </x-slot:middle>
+        <x-slot:actions>
+            <x-button label="Filters" @click="$wire.drawer = true" responsive icon="o-funnel" badge badge-classes="badge-warning" />
+        </x-slot:actions>
+    </x-header>
+
+    <!-- TABLE  -->
+    <x-card shadow>
+        <x-table :headers="$headers" :rows="$users" :sort-by="$sortBy" with-pagination link="users/{id}/edit">
+            @scope('cell_avatar', $user)
+            <x-avatar image="{{ $user->avatar ?? '/empty-user.jpg' }}" class="!w-10" />
+            @endscope
+            @scope('actions', $user)
+            <x-button icon="o-trash" wire:click="delete({{ $user['id'] }})" wire:confirm="Are you sure?" spinner class="btn-ghost btn-sm text-error" />
+            @endscope
+        </x-table>
+    </x-card>
+
+    <!-- FILTER DRAWER -->
+    <x-drawer wire:model="drawer" title="Filters" right separator with-close-button class="lg:w-1/3">
+        <div class="grid gap-5">
+            <x-input placeholder="Search..." wire:model.live.debounce="search" icon="o-magnifying-glass" @keydown.enter="$wire.drawer = false" />
+            <x-select placeholder="Country" wire:model.live="country_id" :options="$countries" icon="o-flag" placeholder-value="0" /> 
+            <x-slot:actions>
+                <x-button label="Reset" icon="o-x-mark" wire:click="clear" spinner />
+                <x-button label="Done" icon="o-check" class="btn-primary" @click="$wire.drawer = false" />
+            </x-slot:actions>
+        </div>
+    </x-drawer>
+</div>
